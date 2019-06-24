386cde9b540568d383f6079f2bf4fb79cad4e07f
Talent Antonio <antonio.talenty@gmail.com>
2019-06-24 11:50:19 +0100

    server running

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ server.use(express.json());
 
 // sanity check route
 server.get('/', (req, res) => {
-  res.status(200).json({ hello: 'World!' });
+  res.status(200).json({ Hello: 'Team 10!' });
 });
 
 module.exports = server;
