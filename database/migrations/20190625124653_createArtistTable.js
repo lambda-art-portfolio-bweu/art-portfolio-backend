de1ad241d4daeda6e03a57c8a3eb73b65afc0ee6
Talent Antonio <antonio.talenty@gmail.com>
2019-06-25 12:53:28 +0100

    data seed and fix from Maxime comments

--- database/migrations/20190625124653_createArtistTable.js
+++ database/migrations/20190625124653_createArtistTable.js
@@ -1,22 +1,28 @@
-
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('artist', (artist) => {
         artist.increments();
         artist
-        .string('name', 128)
+        .string('artistName', 128)
         .notNullable()
 
+        artist.string('artistDescription', 255)
+        .notNullable()
+        
         artist
-        .string('lastname', 128)
+        .string('profilePictureUrl')
         .notNullable()
-
+        
         artist
         .string('username', 128)
         .notNullable()
+
+        artist
+        .string('email', 128)
+        .notNullable()
+
         artist.string('password', 128)
              .notNullable();
-        artist.string('description', 552)
-             .notNullable();
+      
         artist.timestamps(true,true)
       });
   
