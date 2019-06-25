b780e9b294d849db839c1d3aa1994a9439d959d5
Talent Antonio <antonio.talenty@gmail.com>
2019-06-25 08:27:53 +0100

    knex file update

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,20 @@
-// Update with your config settings.
-
 module.exports = {
-
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './database/auth3.db3',
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
   },
-  production: {
-    client: 'sqlite3',
-    connection: process.env.DATABASE_URL
-  }
 };
