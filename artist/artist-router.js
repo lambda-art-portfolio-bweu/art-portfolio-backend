f262670a2ececc9ead9d9463b94b647dcb0a7698
Talent Antonio <antonio.talenty@gmail.com>
2019-06-26 02:11:42 +0100

    get and get by ID

--- artist/artist-router.js
+++ artist/artist-router.js
@@ -2,3 +2,28 @@ const router = require('express').Router();
 
 const Artist = require('../artist/artist-model.js');
 const restricted = require('../auth/restricted-middleware.js');
+
+
+router.get('/', async (req, res) => {
+    try {
+      const allUsers = await Artist.findAll();
+      res.status(200).json(allUsers);
+    } catch(error) {
+      res.status(500).json({ error: "There was an error getting users from the database" })
+    }
+  });
+  
+  router.get('/:id', async (req, res) => {
+    const id = req.params.id
+    try {
+      const user = await Artist.findById(id);
+      if (user.length > 0) {
+        res.status(200).json(user[0]);
+      } else {
+        res.status(404).json({ error: `User with id ${id} does not exist`})
+      }
+    } catch(error) {
+      res.status(500).json({ error: "There was an error getting the adventure from the database" })
+    }
+  });
+  
