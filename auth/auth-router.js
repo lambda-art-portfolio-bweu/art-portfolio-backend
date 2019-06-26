375dbd49f66a1790a51f0930873babf6483ac362
Talent Antonio <antonio.talenty@gmail.com>
2019-06-26 15:11:26 +0100

    generate token for new user

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -7,6 +7,17 @@ const jwt =  require('jsonwebtoken');
 const secret = require('../config/secrets')
 
 const  Artist = require('../artist/artist-model.js');
+function generateToken(artist){
+  const payload = {
+    subject: artist.id,
+    username: artist.username,
+
+  }
+  const options = {
+    expiresIn: '1d'
+  }
+  return jwt.sign(payload, secret.jwtSecret, options)
+}
 
 
 module.exports = router;
