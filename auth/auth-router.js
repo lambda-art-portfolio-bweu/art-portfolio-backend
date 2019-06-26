a74b52e29f4cc446821609b67634eb58aa98a309
Talent Antonio <antonio.talenty@gmail.com>
2019-06-26 02:12:54 +0100

    auth router setup

---
+++ auth/auth-router.js
@@ -0,0 +1,8 @@
+
+
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+
+const  Artists = require('../artist/artist-model.js');
+const hashPassword = require('../helpers/hashPassword');
+const tokenService = require('../auth/token-service.js');
