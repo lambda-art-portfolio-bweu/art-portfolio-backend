4cb95686dc418dbc5b41e85fcd81b3bfcdc7ddba
Talent Antonio <antonio.talenty@gmail.com>
2019-06-26 01:42:25 +0100

    secrete hash password

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -5,3 +5,19 @@ const secrets = require('../config/secrets.js');
 
 module.exports = (req, res, next) => {
   const token = req.headers.authorization;
+
+  const theSecrete = process.env.SECRET || 'Team10'
+
+  if (token) {
+    jwt.verify(token, theSecrete, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ You: "Can't touch this!" });
+      } else {
+        req.decodedJwt = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ You: 'Shall not pass!' });
+  }
+};
