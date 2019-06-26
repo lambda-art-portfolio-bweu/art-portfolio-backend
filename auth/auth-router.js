e2fd56ca5252762b5f8bcddb70da2608f52e8303
Talent Antonio <antonio.talenty@gmail.com>
2019-06-26 02:13:34 +0100

    register artist route / end point

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -6,3 +6,23 @@ const bcrypt = require('bcryptjs');
 const  Artists = require('../artist/artist-model.js');
 const hashPassword = require('../helpers/hashPassword');
 const tokenService = require('../auth/token-service.js');
+
+
+router.post('/register', (req, res) => {
+  let artist = req.body;
+  const hash = hashPassword(artist.password);
+  artist.password = hash;
+
+  Artists.add(artist)
+    .then(saved => {
+      const token = tokenService.makeTokenFromArtist(saved);
+      res.status(201).json({
+        saved,
+        id: saved[0].id,
+        token
+      });
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
