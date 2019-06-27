51d43167bba2323a78f5df4cb93e8f3c69e4b587
Talent Antonio <antonio.talenty@gmail.com>
2019-06-27 12:47:31 +0100

    get Posts

--- posts/post-router.js
+++ posts/post-router.js
@@ -4,3 +4,13 @@ const Posts = require("./post-model.js");
 const router = express.Router();
 
 router.use(express.json());
+
+router.get("/", (req, res) => {
+    Posts.get()
+      .then((data) => res.json(data))
+      .catch((data) =>
+        res.status(500).json({
+          errorMessage: "The post could not be retrieved."
+        })
+      );
+  });
