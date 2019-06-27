45cfdbf07d3e4f4933dfc6fc9d2697873a96dc84
Talent Antonio <antonio.talenty@gmail.com>
2019-06-27 12:43:07 +0100

    insert / update and remove

--- posts/post-model.js
+++ posts/post-model.js
@@ -18,3 +18,23 @@ function get() {
       .first();
   }
   
+  function insert(post) {
+    return db('posts')
+      .insert(post)
+      .then(ids => {
+        return getById(ids[0]);
+      });
+  }
+  
+  function update(id, changes) {
+    return db('posts')
+      .where({ id })
+      .update(changes);
+  }
+  
+  function remove(id) {
+    return db('posts')
+      .where('id', id)
+      .del();
+  }
+  
