b1c40d1d627edd4a3e44fc308773468893543420
Talent Antonio <antonio.talenty@gmail.com>
2019-06-27 12:36:55 +0100

    seeded data for post

--- database/migrations/20190627093901_createPostsTable.js
+++ database/migrations/20190627093901_createPostsTable.js
@@ -1,37 +1,33 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('posts', (posts) => {
         posts.increments();
+
         posts
         .string('name', 128)
         .notNullable()
+        .unique()
 
         posts
         .text('description')
         .notNullable();
 
-        posts
-        .string('categories')
+        posts.string('categories')
         .notNullable();
  
-        posts
-        .string('pictureUrl')
+        posts.string('pictureUrl')
         .notNullable()
 
         posts
         .integer('artist_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
 
         posts
         .integer('heart')
 
         posts.timestamps(true,true)
       });
-//       post: {
-//         name: '',
-//         description:'',**
-//         categories: [],
-//         pictureUrl: '',
-//         artistID: '',**
-//       }
 };
 
 exports.down = function(knex, Promise) {
