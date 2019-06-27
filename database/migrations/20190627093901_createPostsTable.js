cf8227cc3cd915c68bbf02a4e2e64ec90b1d41f9
Talent Antonio <antonio.talenty@gmail.com>
2019-06-27 10:03:12 +0100

    Posts schema table done

--- database/migrations/20190627093901_createPostsTable.js
+++ database/migrations/20190627093901_createPostsTable.js
@@ -1,8 +1,37 @@
-
 exports.up = function(knex, Promise) {
-  
-};
+    return knex.schema.createTable('posts', (posts) => {
+        posts.increments();
+        posts
+        .string('name', 128)
+        .notNullable()
+
+        posts
+        .text('description')
+        .notNullable();
+
+        posts
+        .string('categories')
+        .notNullable();
+ 
+        posts
+        .string('pictureUrl')
+        .notNullable()
+
+        posts
+        .integer('artist_id')
+
+        posts.timestamps(true,true)
+      });
+//       post: {
+//         name: '',
+//         description:'',**
+//         categories: [],
+//         pictureUrl: '',
+//         artistID: '',**
+//       }
+// };
 
 exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('posts');
   
 };
