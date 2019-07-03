c81ac661923559d5e30695932294ee8a5bdd4b29
ruihildt <ruihildt@protonmail.com>
2019-07-03 22:35:39 +0200

    when updating artist, send back the changed artist as an object

--- database/seeds/01-artist.js
+++ database/seeds/01-artist.js
@@ -1,7 +1,6 @@
 exports.seed = function(knex, Promise) {
-
   // Deletes ALL existing entries
-  return knex('artist').del()
+  return knex('table_name').del()
     .then(function () {
       // Inserts seed entries
       return knex("artist").insert([
