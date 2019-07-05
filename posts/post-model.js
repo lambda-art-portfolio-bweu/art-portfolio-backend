3bf4807ac0c86b8a32a153f6070d138a2f4e8990
Ikechukwu <iykekings36@gmail.com>
2019-07-05 18:27:00 +0100

    getPostById works with uername now

--- posts/post-model.js
+++ posts/post-model.js
@@ -6,35 +6,39 @@ module.exports = {
   insert,
   update,
   remove,
+  findBy
 };
 
 function get() {
-    return db('posts');
-  }
-  
-  function getById(id) {
-    return db('posts')
-      .where({ id })
-      .first();
-  }
-  
-  function insert(post) {
-    return db('posts')
-      .insert(post)
-      .then(ids => {
-        return getById(ids[0]);
-      });
-  }
-  
-  function update(id, changes) {
-    return db('posts')
-      .where({ id })
-      .update(changes);
-  }
-  
-  function remove(id) {
-    return db('posts')
-      .where('id', id)
-      .del();
-  }
-  
+  return db('posts');
+}
+
+function getById(id) {
+  return db('posts')
+    .where({ id })
+    .first();
+}
+function findBy(name) {
+  return db('posts')
+    .where({ name })
+    .first();
+}
+function insert(post) {
+  return db('posts')
+    .insert(post)
+    .then(ids => {
+      return getById(ids[0]);
+    });
+}
+
+function update(id, changes) {
+  return db('posts')
+    .where({ id })
+    .update(changes);
+}
+
+function remove(id) {
+  return db('posts')
+    .where('id', id)
+    .del();
+}
